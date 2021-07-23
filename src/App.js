fcf8ac559c3a06210f0a89030c44062000e787ab
Emily Gleeson <engleeson@gmail.com>
2021-07-22 22:29:43 -0400

    rendering footeR

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import projects from './utils/projects';
 import Header from './components/Header';
 import Project from './components/Project';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
       <Project projects={projects}/>
       <Project projects={projects}/>
       <Project projects={projects}/> */}
+
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,11 @@
-import React from 'React'
+import React from 'react'
 
 //component with footer from my site
+
+export default function Footer() {
+    return (
+        <footer>
+        Thanks for visiting!! 💖
+        </footer>
+    )
+}
